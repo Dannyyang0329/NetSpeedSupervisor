f16a23d1c2717faed572db5831f63cadb7234b9f
Dannyyang0329 <f74094067@gs.ncku.edu.tw>
2022-08-16 21:31:59 +0800

    docs: change period unit to minute

--- config_window.py
+++ config_window.py
@@ -16,8 +16,8 @@ def save_config(p_text, u_text, d_text):
 
         # save the setting if values are valid
         with open('config.txt', 'w') as f:
-            str = pt + " " + ut + " " + dt
-            f.write(str)
+            tmp = str(p_val * 60) + " " + ut + " " + dt
+            f.write(tmp)
     except:
         messagebox.showinfo("Error", "Value is invalid")
 
@@ -28,52 +28,52 @@ def get_config_setting(window):
     # open sub window for setting
     setting_window = Toplevel()
     setting_window.title('Setting')
-    setting_window.geometry('400x245+320+220')
+    setting_window.geometry('460x245+320+220')
     setting_window.resizable(width=False, height=False)
     setting_window['background'] = '#141526'
 
     ONE_PIXEL = PhotoImage(width=1, height=1)
 
     # period
-    p_title = Label(setting_window, width=220, height=40, image=ONE_PIXEL)
-    p_title['text'] = 'Period'
-    p_title['font'] = tkf.Font(family='Ubuntu Mono', size=18, weight='bold')
+    p_title = Label(setting_window, width=270, height=40, image=ONE_PIXEL)
+    p_title['text'] = 'Period (minute)'
+    p_title['font'] = tkf.Font(family='Ubuntu Mono', size=16, weight='bold')
     p_title['compound'] = CENTER
     p_title['background'] = '#141526'
     p_title['foreground'] = '#b7c2cc'
-    p_title.place(x=20, y=20)
+    p_title.place(x=25, y=20)
 
     p_text = Text(setting_window, width=8, height=1)
-    p_text['font'] = tkf.Font(family="Ubuntu Mono", size=14)
-    p_text.place(x=280, y=20)
+    p_text['font'] = tkf.Font(family="Ubuntu Mono", size=16)
+    p_text.place(x=330, y=30)
 
 
     # upload limit
-    u_title = Label(setting_window, width=220, height=40, image=ONE_PIXEL)
-    u_title['text'] = 'Upload min value'
-    u_title['font'] = tkf.Font(family='Ubuntu Mono', size=18, weight='bold')
+    u_title = Label(setting_window, width=270, height=40, image=ONE_PIXEL)
+    u_title['text'] = 'Upload min speed (Mbps)'
+    u_title['font'] = tkf.Font(family='Ubuntu Mono', size=16, weight='bold')
     u_title['compound'] = CENTER
     u_title['background'] = '#141526'
     u_title['foreground'] = '#b7c2cc'
-    u_title.place(x=20, y=60)
+    u_title.place(x=25, y=60)
 
     u_text = Text(setting_window, width=8, height=1)
-    u_text['font'] = tkf.Font(family="Ubuntu Mono", size=14)
-    u_text.place(x=280, y=60)
+    u_text['font'] = tkf.Font(family="Ubuntu Mono", size=16)
+    u_text.place(x=330, y=70)
 
 
     # download limit
-    d_title = Label(setting_window, width=220, height=40, image=ONE_PIXEL)
-    d_title['text'] = 'Download min value'
-    d_title['font'] = tkf.Font(family='Ubuntu Mono', size=18, weight='bold')
+    d_title = Label(setting_window, width=270, height=40, image=ONE_PIXEL)
+    d_title['text'] = 'Download min speed (Mbps)'
+    d_title['font'] = tkf.Font(family='Ubuntu Mono', size=16, weight='bold')
     d_title['compound'] = CENTER
     d_title['background'] = '#141526'
     d_title['foreground'] = '#b7c2cc'
-    d_title.place(x=20, y=100)
+    d_title.place(x=25, y=100)
 
     d_text = Text(setting_window, width=8, height=1)
-    d_text['font'] = tkf.Font(family="Ubuntu Mono", size=14)
-    d_text.place(x=280, y=100)
+    d_text['font'] = tkf.Font(family="Ubuntu Mono", size=16)
+    d_text.place(x=330, y=110)
 
 
     # save button
@@ -81,7 +81,7 @@ def get_config_setting(window):
     save_btn['text'] = 'Save'
     save_btn['font'] = tkf.Font(family='Ubuntu Mono', size=24, weight='bold')
     save_btn.configure(bd=0, highlightthickness=0, compound=CENTER, background='#636ff7', activebackground='#89b4fa')
-    save_btn.place(x=140, y=160)
+    save_btn.place(x=170, y=160)
 
 
     setting_window.transient(window)
--- main_window.py
+++ main_window.py
@@ -92,9 +92,9 @@ def set_config(init = False):
     # update the global variable
     with open('config.txt', 'r') as f:
         period, upload_min_value, download_min_value = [float(val) for val in f.readline().split()]
-        p_status['text'] = "Period   : " + str(period)
-        d_status['text'] = "Download : " + str(download_min_value)
-        u_status['text'] = "Upload   : " + str(upload_min_value)
+        p_status['text'] = period / 60
+        d_status['text'] = download_min_value
+        u_status['text'] = upload_min_value
 
     # restart when configuration is modified
     if init == False:
@@ -107,7 +107,7 @@ def set_config(init = False):
 # GUI window
 window = Tk()
 window.title("Network Speed Supervisor")
-window.geometry("440x260+300+200")
+window.geometry("500x260+300+200")
 window.resizable(width = False, height = False)
 window['background'] = '#141526'
 
@@ -120,7 +120,7 @@ download_title['font'] = tkf.Font(family='Ubuntu Mono', size=18, weight='bold')
 download_title['compound'] = CENTER
 download_title['background'] = '#141526'
 download_title['foreground'] = '#b7c2cc'
-download_title.place(x=10, y=20)
+download_title.place(x=50, y=20)
 
 download_num = Label(window, width=200, height=60, image=ONE_PIXEL)
 download_num['text'] = '0.00'
@@ -128,7 +128,7 @@ download_num['font'] = tkf.Font(family='Ubuntu Mono', size=38, weight='bold')
 download_num['compound'] = CENTER
 download_num['background'] = '#141526'
 download_num['foreground'] = '#666875'
-download_num.place(x=10, y=75)
+download_num.place(x=50, y=75)
 
 
 # Uplaod
@@ -138,7 +138,7 @@ upload_title['font'] = tkf.Font(family='Ubuntu Mono', size=18, weight='bold')
 upload_title['compound'] = CENTER
 upload_title['background'] = '#141526'
 upload_title['foreground'] = '#b7c2cc'
-upload_title.place(x=220, y=20)
+upload_title.place(x=280, y=20)
 
 upload_num = Label(window, width=200, height=60, image=ONE_PIXEL)
 upload_num['text'] = '0.00'
@@ -146,7 +146,7 @@ upload_num['font'] = tkf.Font(family='Ubuntu Mono', size=38, weight='bold')
 upload_num['compound'] = CENTER
 upload_num['background'] = '#141526'
 upload_num['foreground'] = '#666875'
-upload_num.place(x=220, y=75)
+upload_num.place(x=280, y=75)
 
 
 # testing button
@@ -154,7 +154,7 @@ test_btn = Button(window, width=140, height=48, image=ONE_PIXEL, command=testing
 test_btn['text'] = 'Test'
 test_btn['font'] = tkf.Font(family='Ubuntu Mono', size=24, weight='bold')
 test_btn.configure(bd=0, highlightthickness=0, compound=CENTER, background='#636ff7', activebackground='#89b4fa')
-test_btn.place(x=150, y=160)
+test_btn.place(x=190, y=160)
 
 
 # setting button
@@ -163,33 +163,54 @@ config_btn['text'] = '⚙'
 config_btn['font'] = tkf.Font(family='Ubuntu Mono', size=20, weight='bold')
 config_btn['foreground'] = '#b7c2cc'
 config_btn.configure(bd=0, highlightthickness=0, compound=CENTER, background='#141526', activebackground='#141526')
-config_btn.place(x=380, y=210)
+config_btn.place(x=440, y=210)
 
 
 # Status
-p_status = Label(window, width=130, height=20, image=ONE_PIXEL)
+p_label = Label(window, width=70, height=20, image=ONE_PIXEL)
+p_label['text'] = 'Period   :'
+p_label['font'] = tkf.Font(family='Ubuntu Mono', size=10, weight='bold')
+p_label['compound'] = CENTER
+p_label['background'] = '#141526'
+p_label['foreground'] = '#b7c2cc'
+p_label.place(x=5, y=160)
+p_status = Label(window, width=60, height=20, image=ONE_PIXEL)
 p_status['text'] = 'Period   : loading'
 p_status['font'] = tkf.Font(family='Ubuntu Mono', size=10, weight='bold')
 p_status['compound'] = CENTER
 p_status['background'] = '#141526'
 p_status['foreground'] = '#b7c2cc'
-p_status.place(x=5, y=160)
+p_status.place(x=80, y=160)
 
-d_status = Label(window, width=130, height=20, image=ONE_PIXEL)
+d_label = Label(window, width=70, height=20, image=ONE_PIXEL)
+d_label['text'] = 'Download :'
+d_label['font'] = tkf.Font(family='Ubuntu Mono', size=10, weight='bold')
+d_label['compound'] = CENTER
+d_label['background'] = '#141526'
+d_label['foreground'] = '#b7c2cc'
+d_label.place(x=5, y=180)
+d_status = Label(window, width=60, height=20, image=ONE_PIXEL)
 d_status['text'] = 'Download : loading'
 d_status['font'] = tkf.Font(family='Ubuntu Mono', size=10, weight='bold')
 d_status['compound'] = CENTER
 d_status['background'] = '#141526'
 d_status['foreground'] = '#b7c2cc'
-d_status.place(x=5, y=180)
+d_status.place(x=80, y=180)
 
-u_status = Label(window, width=130, height=20, image=ONE_PIXEL)
+u_label = Label(window, width=70, height=20, image=ONE_PIXEL)
+u_label['text'] = 'Upload   :'
+u_label['font'] = tkf.Font(family='Ubuntu Mono', size=10, weight='bold')
+u_label['compound'] = CENTER
+u_label['background'] = '#141526'
+u_label['foreground'] = '#b7c2cc'
+u_label.place(x=5, y=200)
+u_status = Label(window, width=60, height=20, image=ONE_PIXEL)
 u_status['text'] = 'Upload   : loading'
 u_status['font'] = tkf.Font(family='Ubuntu Mono', size=10, weight='bold')
 u_status['compound'] = CENTER
 u_status['background'] = '#141526'
 u_status['foreground'] = '#b7c2cc'
-u_status.place(x=5, y=200)
+u_status.place(x=80, y=200)
 
  # signature
 signature_label = Label(window, image=ONE_PIXEL)
@@ -197,7 +218,7 @@ signature_label['text'] = 'Created by Dannyyang0329'
 signature_label['font'] = tkf.Font(family='Ubuntu Mono', size=10, slant='italic')
 signature_label['foreground'] = '#d9d9d9'
 signature_label.configure(compound=CENTER, background='#141526')
-signature_label.place(x=10, y=230)
+signature_label.place(x=5, y=230)
 
 
 set_config(init=True)
